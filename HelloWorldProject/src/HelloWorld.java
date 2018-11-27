5ec11fb7b7d75faa6f1e919f04f73416034ae96d
georg.gornik <georg.gornik@KK-GYM-EDV1-08.gym.kk.local>
2018-11-27 17:42:34 +0100

    Kommentare hinzufügen

--- HelloWorldProject/src/HelloWorld.java
+++ HelloWorldProject/src/HelloWorld.java
@@ -3,8 +3,14 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-          System.out.println ("Hallo Klasse");
-             
+		System.out.println("Hallo Klasse");
+		// das ist ein kommentar. kein code der ausgeführt wird
+		// einzeiliger kommentar
+		/*
+		 * mehr zeiliger kommentar kommentar
+		 * hallo
+		 */
+
 	}
 
 }
