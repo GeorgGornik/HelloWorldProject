dbe80c812d35c8dfbc139bca0f3a37dfae531f82
georg.gornik <georg.gornik@KK-GYM-EDV1-08.gym.kk.local>
2019-03-19 18:05:15 +0100

    <rzew

--- HelloWorldProject/src/sortieren/Quicksort.java
+++ HelloWorldProject/src/sortieren/Quicksort.java
@@ -8,14 +8,14 @@ public class Quicksort {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		for (int n = zahlen.length; n > 1; --n) {
-		}
+		for (int i : zahlen)
+			System.out.println(i);
+
 		
-		quicksort(0, zahlen.length -1);
-		 for (int i : zahlen)
-			 System.out.println(i);
-		
-		
+		quicksort(0, zahlen.length - 1);
+		for (int i : zahlen)
+			System.out.println(i);
+
 	}
 
 	static void quicksort(int links, int rechts) {
@@ -29,7 +29,9 @@ public class Quicksort {
 			quicksort(links, teiler - 1);
 
 			quicksort(teiler + 1, rechts);
-
+            
+		   
+			 
 		}
 
 	}
@@ -38,9 +40,35 @@ public class Quicksort {
 		int i = links;
 		int j = rechts - 1;
 		int pivot = zahlen[rechts];
-		
-		
-		
+
+		// Suche von links ein Element, welches größer als das Pivotelement ist
+		do {
+
+			while (i < j && zahlen[i] > pivot) {
+				i = i + 1;
+
+			}
+
+			// Suche von rechts ein Element, welches gleich oder kleiner als das
+			// Pivotelement ist
+
+			while (j > i && zahlen[j] >= pivot) {
+				j = j - 1;
+			}
+
+			if (i < j) {
+				int help = zahlen[i];
+				zahlen[i] = zahlen[j];
+				zahlen[j] = help;
+			}
+		} while (i < j);
+
+		if (zahlen[i] > pivot) {
+			int help = zahlen[i];
+			zahlen[i] = pivot;
+			zahlen[rechts] = help;
+		}
+
 		return i;
 
 	}
